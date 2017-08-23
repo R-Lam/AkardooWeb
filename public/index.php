c32930ddab249e4154c7faca0a2ca3ab5f8198f6
Erick Arévalo <kerick.fabricio@gmail.com>
2017-08-23 16:10:47 -0600

    Guias para diseño

--- public/index.php
+++ public/index.php
@@ -1,5 +1,45 @@
 <!-- Se incluye el encabezado de la pagina -->
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/inc/header.php') ?>
 
+<!-- Titulo y contenido principal -->
+
+<div class="principal">
+    <div class="row">
+       <div class="col s12 m8">
+            <div class="cont_left bg_white">
+                <div class="st_title"> ¡Noticias! </div>
+                <!-- Entrada al blog en pantalla completa -->
+                <div class="title_blog">
+                    <div class="avatar-left tooltipped" data-position="top" data-delay="50" data-tooltip="MegaZeroxs">
+                        <a href="#"><img class="s_avatar_img" src="/img/avatars/default.png" /></a>
+                    </div>
+                    <!-- Flecha -->
+                    <div class="arrow"><i class="material-icons">play_arrow</i></div>
+                    <div class="title_right">
+                        <span class="title_date right">23/08/2017</span>
+                        <span class="title_nick">Actualización de prueba en el 23 de agosto de 2017</span><br>
+                    </div>
+                </div>
+                <!-- Entrada al blog en pantallas pequeñas -->
+                <div>
+                    Hola akardeanos, me complace anunciarles los cambios mas recientes dentro del servidor survival:
+
+Los VIP podrán acceder a kits exclusivos cada semana, dependiendo del rango mejor serán los objetos obtenidos mediante el kit.
+Para fomentar el uso de las elitras, cada kit exclusivo de VIP cuentan con cohetes que incrementan su poder según el rango del VIP, ademas de cuero para reparar tu elitra.
+Cuando un usuario es asesinado tiene una probabilidad de 20% de soltar su cabeza, mas sin embargo, si matas a un VIP, dicha probabilidad disminuirá progresivamente hasta un 5% con el VIP Diamond.
+Ya hace un día que el survival cuenta con Jobs! Asi incrementaras las posibilidades de ser el mas rico del servidor.
+Con el mas reciente cambio en los Jobs, ahora los VIP podrán acceder a 2 trabajos a la vez e inclusive podrán llevarlos mas allá... Podran subir dichos trabajos hasta el nivel 12.
+De momento estos han sido los cambios mas recientes realizados, los estaremos informando en futuras mejoras y novedades :)
+                </div>
+            </div>
+       </div>
+       <div class="col s12 m4">
+            <div class="cont-right bg_white">
+                Lorem ipsum
+            </div>
+       </div>
+    </div>
+</div>
+
 <!-- Se incluye el footer de la pagina -->
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/inc/footer.php') ?>
